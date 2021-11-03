cd00ddd37aa4591223a3a047b3fdc3f30ab06077
rockywu 吴佳雷 <wjl19890427@hotmail.com>
2021-11-03 17:06:03 +0800

    初步解决for逻辑渲染

--- src/index.js
+++ src/index.js
@@ -9,25 +9,67 @@ node2fragment(document.getElementById('app'))
 
 //将ast转变为dom节点
 
-function renderAst(ast, obj) { 
+function renderAst(ast, obj, exts) { 
+  exts = exts || {}
   let el;
   if(ast.type === 2) {
     //文本直接渲染
     if(ast.parse) {
-      let val = new Function("return " + ast.parse.expression).call(obj)
+      let keys = Object.keys(exts).map(key => key);
+      let values = Object.values(exts);
+      let val;
+      console.log("keys", keys, values)
+      if(keys.length == 2) {
+        val = new Function(keys[0], keys[1], "return " + ast.parse.expression).apply(obj, values)
+      } else {
+        val = new Function("return " + ast.parse.expression).call(obj)
+      }
+      console.log("aaaa", val)
       el = document.createTextNode(val)
     } else {
       el = document.createTextNode(ast.text)
     }
   } else {
+    let vfor = null;
+    vfor = ast.attrs.find(attr => {
+      return attr.name == 'v-for' && attr.parse && attr.parse.for ? true : false
+    })
     //节点类型进行创建
-    el = document.createElement(ast.tag);
-    ast.attrs.map(attr => {
-      el.setAttribute(attr.name, attr.value)
-    })
-    ast.children.map(child => {
-      el.appendChild(renderAst(child, obj))
-    })
+    function renderOne(exts) {
+      var elOne = document.createElement(ast.tag);
+      // let vfor = null;
+      ast.attrs.map(attr => {
+        if(!(attr.parse && attr.parse.for))  {
+          elOne.setAttribute(attr.name, attr.value)
+        }
+      })
+      ast.children.map(child => {
+        let tmps = renderAst(child, obj, exts)
+        if(!(tmps instanceof Array)) {
+          tmps = [tmps];
+        }
+        tmps.map(tmp => {
+          elOne.appendChild(tmp)
+        })
+      })
+      return elOne;
+    }
+    if(vfor) {
+      console.log("vfor.parse", vfor.parse)
+      let vforParse = vfor.parse;
+      let forData = new Function("return " + vfor.parse.for).call(obj);
+      // tmpExts[]
+      el = Object.keys(forData).map(key => {
+        let val = forData[key];
+        let tmpExts = {
+          [vforParse.alias]: val,
+          [vforParse.iterator1 || 'index']: key
+        };
+        return renderOne(tmpExts)
+      })
+    } else {
+      return renderOne()
+    }
   }
   return el;
 }
@@ -36,12 +78,17 @@ let obj = {
   age: "我的年龄",
   info: "我的信息",
   name: "我的名字",
+  list: {
+    a: "a1",
+    b: "b2",
+    c: "c3",
+    d: "d4"
+  },
   filters: {
     format(value, ...args) {
       console.log('format', value, args)
       return value;
     }
-
   },
   __rf(name) {
     if(typeof this.filters[name] === 'function') {
@@ -52,17 +99,11 @@ let obj = {
   },
   __rt(value) {
     return value;
-  }
+  },
 };
 function replaceNode(oldNode, newNode) {
-  console.log("newNode", newNode)
   oldNode.parentNode.replaceChild(newNode, oldNode)
 }
 replaceNode(dom, renderAst(ast, obj))
 
-setTimeout(() => {
-  obj.age = "我的年龄是19"
-  console.log('aaa', html, ast)
-  replaceNode(document.getElementById('app'), renderAst(ast, obj))
-}, 2000)
 
--- src/parser/index.js
+++ src/parser/index.js
@@ -5,6 +5,28 @@ import {parseFilters} from './filter-parser'
 import {parseText} from './text-parser'
 import {HTMLtoAST} from './html-parser'
 export const bindRE = /^:|^\.|^v-bind:/
+export const forAliasRE = /([\s\S]*?)\s+(?:in|of)\s+([\s\S]*)/
+export const forIteratorRE = /,([^,\}\]]*)(?:,([^,\}\]]*))?$/
+const stripParensRE = /^\(|\)$/g
+
+export function parseFor (exp){
+  const inMatch = exp.match(forAliasRE)
+  if (!inMatch) return
+  const res = {}
+  res.for = inMatch[2].trim()
+  const alias = inMatch[1].trim().replace(stripParensRE, '')
+  const iteratorMatch = alias.match(forIteratorRE)
+  if (iteratorMatch) {
+    res.alias = alias.replace(forIteratorRE, '').trim()
+    res.iterator1 = iteratorMatch[1].trim()
+    if (iteratorMatch[2]) {
+      res.iterator2 = iteratorMatch[2].trim()
+    }
+  } else {
+    res.alias = alias
+  }
+  return res
+}
 
 function formatAst(ast) {
   if(ast.type === 2) {
@@ -15,7 +37,11 @@ function formatAst(ast) {
       return formatAst(one)
     })
     ast.attrs = ast.attrs.map(attr => {
-      if(bindRE.test(attr.name)) {
+      if(attr.name == "v-for") {
+        //v-for语句
+        attr.parse = parseFor(attr.value);
+        console.log("v-for", attr.parse)
+      } else if(bindRE.test(attr.name)) {
         //需要解析
         attr.parse = parseFilters(attr.value)
       } else {
